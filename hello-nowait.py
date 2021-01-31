4711fa0669d6491e0bf27cdb17a569ae5628e2b4
gavrun <gavrunov@live.com>
2021-01-31 14:33:25 +0300

    Add project files.

---
+++ hello-nowait.py
@@ -0,0 +1,2 @@
+name = input('Hello user! What is your name? \n')
+print('Hello %s! How can I help?' % name)
---
+++ hello.py
@@ -0,0 +1,3 @@
+name = input('Hello user! What is your name? \n')
+print('Hello %s! How can I help?' % name)
+input('Press Enter to Exit...')
